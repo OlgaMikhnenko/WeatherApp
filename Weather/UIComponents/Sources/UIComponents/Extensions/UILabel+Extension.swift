d8897e5a91ef9b7cc8643295ba390564eb37956b
Olga Mikhnenko <olga230397@icloud.com>
2022-12-04 16:58:49 +0100

    fix label shadows

--- Weather/UIComponents/Sources/UIComponents/Extensions/UILabel+Extension.swift
+++ Weather/UIComponents/Sources/UIComponents/Extensions/UILabel+Extension.swift
@@ -11,12 +11,11 @@ public extension UILabel {
     convenience init(withShadow: Bool) {
         self.init()
         guard withShadow else { return }
-        layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.8).cgColor
+        layer.shadowColor = UIColor.black.withAlphaComponent(0.8).cgColor
         layer.shadowOpacity = 1
         layer.shadowRadius = 3
-        layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.shadowOffset = CGSize(width: 2, height: 2)
         layer.masksToBounds = false
-        layer.shadowPath = UIBezierPath(rect: bounds).cgPath
     }
 }
 
